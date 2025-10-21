b2bfc2e40cde026a8e0c34e78e8aff9b26af9c15
i7620-lgtm <i7620@guru.sd.belajar.id>
2025-10-21 15:22:03 +0800

    Update and rename p5data.js to p5data.tsx

--- components/p5data.tsx
+++ components/p5data.tsx
@@ -1,100 +1,98 @@
-// Structure: Record<Dimension, Record<Element, SubElement[]>>
-// User's terms: Dimensi -> Sub Elemen -> Target Pencapaian
-// Official terms: Dimensi -> Elemen -> Subelemen
-export const P5_DATA: Record<string, Record<string, string[]>> = {
+export const P5_DATA = {
   "Beriman, bertakwa kepada Tuhan YME, dan berakhlak mulia": {
     "Akhlak Beragama": [
       "Mengenal dan Mencintai Tuhan Yang Maha Esa",
       "Pemahaman Agama/Kepercayaan",
-      "Pelaksanaan Ritual Ibadah",
+      "Pelaksanaan Ritual Ibadah"
     ],
     "Akhlak Pribadi": [
       "Integritas",
-      "Merawat Diri secara Fisik, Mental, dan Spiritual",
+      "Merawat Diri secara Fisik, Mental, dan Spiritual"
     ],
     "Akhlak kepada Manusia": [
       "Mengutamakan persamaan dengan orang lain dan menghargai perbedaan",
-      "Berempati kepada orang lain",
+      "Berempati kepada orang lain"
     ],
     "Akhlak kepada Alam": [
       "Memahami Keterhubungan Ekosistem Bumi",
-      "Menjaga Lingkungan Alam Sekitar",
+      "Menjaga Lingkungan Alam Sekitar"
     ],
     "Akhlak Bernegara": [
-      "Melaksanakan Hak dan Kewajiban sebagai Warga Negara Indonesia",
-    ],
+      "Melaksanakan Hak dan Kewajiban sebagai Warga Negara Indonesia"
+    ]
   },
   "Berkebinekaan global": {
     "Mengenal dan menghargai budaya": [
       "Mendalami budaya dan identitas budaya",
       "Mengeksplorasi dan membandingkan pengetahuan budaya, kepercayaan, serta praktiknya",
-      "Menumbuhkan rasa menghormati terhadap keanekaragaman budaya",
+      "Menumbuhkan rasa menghormati terhadap keanekaragaman budaya"
     ],
     "Komunikasi dan interaksi antar budaya": [
       "Berkomunikasi antar budaya",
-      "Mempertimbangkan dan menumbuhkan berbagai perspektif",
+      "Mempertimbangkan dan menumbuhkan berbagai perspektif"
     ],
     "Refleksi dan tanggung jawab terhadap pengalaman kebinekaan": [
-      "Refleksi terhadap pengalaman kebinekaan.",
+      "Refleksi terhadap pengalaman kebinekaan",
       "Menghilangkan stereotip dan prasangka",
-      "Menyelaraskan perbedaan budaya",
+      "Menyelaraskan perbedaan budaya"
     ],
     "Berkeadilan Sosial": [
-      "Turut serta dalam pembangunan berkelanjutan yang berwawasan lingkungan.",
+      "Turut serta dalam pembangunan berkelanjutan yang berwawasan lingkungan"
     ]
   },
-  "Gotong royong": {
+  "Bergotong-royong": {
     "Kolaborasi": [
       "Kerja sama",
       "Komunikasi untuk mencapai tujuan bersama",
-      "Saling-ketergantungan positif",
-      "Koordinasi sosial"
+      "Saling-ketergantungan positif"
     ],
     "Kepedulian": [
       "Tanggap terhadap lingkungan sosial",
       "Persepsi sosial"
     ],
     "Berbagi": [
-      "Berbagi"
+      "Berbagi informasi, gagasan, dan keterampilan"
     ]
   },
   "Mandiri": {
     "Pemahaman diri dan situasi yang dihadapi": [
       "Mengenali kualitas dan minat diri serta tantangan yang dihadapi",
-      "Mengembangkan refleksi diri",
+      "Mengembangkan refleksi diri"
     ],
     "Regulasi diri": [
       "Regulasi emosi",
       "Penetapan tujuan belajar, prestasi, dan pengembangan diri serta rencana strategis untuk mencapainya",
       "Menunjukkan inisiatif dan bekerja secara mandiri",
       "Mengembangkan pengendalian dan disiplin diri",
-      "Percaya diri, tangguh (resilient), dan adaptif",
-    ],
+      "Percaya diri, resilien, dan adaptif"
+    ]
   },
-  "Bernalar kritis": {
+  "Bernalar Kritis": {
     "Memperoleh dan memproses informasi dan gagasan": [
       "Mengajukan pertanyaan",
-      "Mengidentifikasi, mengklarifikasi, dan mengolah informasi dan gagasan",
+      "Mengidentifikasi, mengklarifikasi, dan mengolah informasi dan gagasan"
     ],
     "Menganalisis dan mengevaluasi penalaran": [
-      "Menganalisis dan mengevaluasi penalaran dan prosedurnya",
+      "Menganalisis dan mengevaluasi penalaran dan prosedurnya"
     ],
     "Merefleksi pemikiran dan proses berpikir": [
-      "Merefleksi dan mengevaluasi pemikirannya sendiri",
+      "Merefleksi dan mengevaluasi pemikirannya sendiri"
     ],
     "Mengambil Keputusan": [
-      "Mengambil keputusan"
+      "Menghasilkan keputusan yang logis dan etis"
     ]
   },
   "Kreatif": {
     "Menghasilkan gagasan yang orisinal": [
-      "Menghasilkan gagasan yang orisinal"
+      "Menghasilkan gagasan yang beragam dan orisinal"
     ],
     "Menghasilkan karya dan tindakan yang orisinal": [
-      "Menghasilkan karya dan tindakan yang orisinal"
+      "Mengeksplorasi dan mengekspresikan pikiran dan/atau perasaannya dalam bentuk karya dan/atau tindakan",
+      "Menghasilkan karya dan tindakan yang orisinal serta inovatif"
     ],
     "Memiliki keluwesan berpikir dalam mencari alternatif solusi permasalahan": [
-        "Memiliki keluwesan berpikir dalam mencari alternatif solusi permasalahan"
+      "Mengidentifikasi masalah dan mencari solusi alternatif",
+      "Mengevaluasi dan memilih solusi terbaik"
     ]
-  },
+  }
 };
