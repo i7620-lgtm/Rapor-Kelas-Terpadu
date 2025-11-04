844057b91eb7130200cb2aa408f70e9ddc101c84
i7620-lgtm <i7620@guru.sd.belajar.id>
2025-11-04 21:55:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import * as App from './App.js';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,6 +10,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   React.createElement(React.StrictMode, null, 
-    React.createElement(App, null)
+    React.createElement(App.default, null)
   )
 );
