55805b91b74b876ef363f86df8187cf2e1e644a3
i7620-lgtm <i7620@guru.sd.belajar.id>
2025-11-16 20:06:03 +0800

    Update PrintPiagamPage.js

--- components/PrintPiagamPage.js
+++ components/PrintPiagamPage.js
@@ -8,7 +8,7 @@ const PAPER_SIZES = {
 };
 
 const PIAGAM_WIDTH = 1115;
-const PIAGAM_HEIGHT = 710; // Further reduced to prevent spilling
+const PIAGAM_HEIGHT = 749; // Further reduced to prevent spilling
 const PIAGAM_VIEWBOX = `0 0 ${PIAGAM_WIDTH} ${PIAGAM_HEIGHT}`;
 
 const toRoman = (num) => {
