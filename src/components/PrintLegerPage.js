c167812d653a9796bbaf8452097ced97e958362e
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-03-17 16:18:14 +0800

    Update PrintLegerPage.js

--- src/components/PrintLegerPage.js
+++ src/components/PrintLegerPage.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect, useRef, useLayoutEffect } from 'react';
+import React, { useMemo, useState, useEffect, useRef, useLayoutEffect } from 'react';  
 import { generateInitialLayout } from './TransliterationUtil.js';
 
 const PAPER_SIZES = {
