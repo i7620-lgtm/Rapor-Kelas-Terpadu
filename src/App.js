39e7cfeb6e4e63a6c8e3eb739007aca06ff4b17d
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-06-02 14:57:20 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect, useRef, useMemo } from 'react';
+import React, { useState, useCallback, useEffect, useRef, useMemo } from 'react'; 
 import * as XLSX from 'xlsx';
 import { NAV_ITEMS, COCURRICULAR_DIMENSIONS } from './constants.js';
 import Navigation from './components/Navigation.js';
