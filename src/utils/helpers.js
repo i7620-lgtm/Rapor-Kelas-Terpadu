1b05669f4e593735184ba4b862c529af66a857a6
i7620-lgtm <i7620@guru.sd.belajar.id>
2026-06-16 19:48:34 +0800

    Create helpers.js

---
+++ src/utils/helpers.js
@@ -0,0 +1,17 @@
+export const getDynamicRKTFileName = (currentSettings) => { 
+    const sanitize = (str) => String(str || '').replace(/[^\w\s-]/g, '').replace(/\s+/g, ' ').trim();
+    const schoolName = sanitize(currentSettings?.nama_sekolah || 'Nama Sekolah');
+    const className = sanitize(currentSettings?.nama_kelas || 'Kelas');
+    const academicYear = sanitize(currentSettings?.tahun_ajaran || 'TA').replace(/\//g, '-');
+    const semester = sanitize(currentSettings?.semester || 'Semester');
+    return `RKT_${schoolName}_${className}_${academicYear}_${semester}.xlsx`.toUpperCase();
+};
+
+export const chunkString = (str, len) => {
+    const size = Math.ceil(str.length / len);
+    const r = Array(size);
+    for (let i = 0; i < size; i++) {
+        r[i] = str.substr(i * len, len);
+    }
+    return r;
+};
